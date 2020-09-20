72fd2c46c3d129a30d9fb2c8814f5f32ff916b20
jonat <jonat@ODIN.local>
2020-09-20 18:51:37 -0300

    Primeiro commit. Estruturando o projeto.

---
+++ src/main/java/com/microservices/localidades/LocalidadesApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.localidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalidadesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalidadesApplication.class, args);
+    }
+
+}
